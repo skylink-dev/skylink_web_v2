12555031e725642eedf7e4cbacaff3b616fee235
Bharanikumar-Skylink <swd-intern01@skylinkfibernet.com>
2025-11-06 14:48:27 +0530

    Added video in support module

--- src/app/(site)/support/page.js
+++ src/app/(site)/support/page.js
@@ -7,11 +7,13 @@ const SliderBanner = dynamic(() => import('@/components/SliderBanner'));
 const SupportSection = dynamic(() => import('@/components/SupportSection'));
 const CustomSearch = dynamic(() => import('@/components/CustomSearch'));
 const ContactDetails = dynamic(() => import('@/components/ContactDetails'));
+const SupportVideos = dynamic(() => import('@/components/SupportVideo'));
 
 export default function Page() {
   return (
     <>
       <SliderBanner />
+      <SupportVideos/>
       <SupportSection
         title="Welcome to Skylink Support"
         subtitle="Want personalized help?"
