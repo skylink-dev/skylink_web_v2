50986c81766d56635b3c77849b7235615da2ca93
GowthamSelvaraj123 <gowthamdesigner94@gmail.com>
2025-11-07 11:15:39 +0530

    Added

--- src/app/(site)/layout.js
+++ src/app/(site)/layout.js
@@ -34,6 +34,10 @@ export default function RootLayout({ children }) {
           crossOrigin="true"
         />
         <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;900&family=Roboto:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+        <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Manrope:wght@200..800&family=Montserrat:wght@100..900&display=swap"
           rel="stylesheet"
         />
