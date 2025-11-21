3f0ed9a13c83002e83393fb5b0be67009f4c43ce
Bharanikumar-Skylink <swd-intern01@skylinkfibernet.com>
2025-11-21 10:17:31 +0530

    Removed loader and navigation error worked

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
     NEXT_PUBLIC_GOOGLE_TAG_KEY: process.env.NEXT_PUBLIC_GOOGLE_TAG_KEY,
   },
+    output: {
+        // Specify the output file tracing root to fix the multiple lockfiles warning
+        fileTracingRoot: process.cwd(),
+  },
 };
 
 export default nextConfig;
